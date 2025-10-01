b2f2eaa4c7b45ad3a6a31e85a7da68008a88f428
apsschotten <ana.schotten@estudante.ifms.edu.br>
2025-10-01 13:21:37 -0400

    01/10 - 13:21
    > Update "index.html".
    > Update "script.js".

---
+++ script.js
@@ -0,0 +1,27 @@
+function somar() {
+    let valorX = document.getElementById('valorX_soma').value;
+    let valorY = document.getElementById('valorY_soma').value;
+    let resultadoSoma = valorX + valorY;
+    document.getElementById('resultado_soma').value = resultadoSoma;
+};
+
+function subtrair() {
+    let valorX = document.getElementById('valorX_sub').value;
+    let valorY = document.getElementById('valorY_sub').value;
+    let resultadoSubtração = valorX - valorY;
+    document.getElementById('resultado_sub').value = resultadoSubtração;
+};
+
+function multiplicar() {
+    let valorX = document.getElementById('valorX_mult').value;
+    let valorY = document.getElementById('valorY_mult').value;
+    let resultadoMultiplicação = valorX * valorY;
+    document.getElementById('resultado_mult').value = resultadoMultiplicação;
+};
+
+function dividir() {
+    let valorX = document.getElementById('valorX_div').value;
+    let valorY = document.getElementById('valorY_div').value;
+    let resultadoDivisão = valorX * valorY;
+    document.getElementById('resultado_div').value = resultadoDivisão;
+};
