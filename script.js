f01352abbd5b01bd813363717b5f4af7335145f3
apsschotten <ana.schotten@estudante.ifms.edu.br>
2025-10-01 13:25:42 -0400

    Bug-fix: Code mistake in "script.js".

--- script.js
+++ script.js
@@ -22,6 +22,6 @@ function multiplicar() {
 function dividir() {
     let valorX = document.getElementById('valorX_div').value;
     let valorY = document.getElementById('valorY_div').value;
-    let resultadoDivisão = valorX * valorY;
+    let resultadoDivisão = valorX / valorY;
     document.getElementById('resultado_div').value = resultadoDivisão;
 };
